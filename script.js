7c5059b67cbaa0cc6b775e1fe2844ed00e36ff10
Don Senior <stepan1q1@gmail.com>
2022-07-08 20:20:58 +0300

    my first commit in this progect

---
+++ script.js
@@ -0,0 +1,55 @@
+
+
+function startGame() {
+	let startButton = $('.startGame');
+	let stopButton = $('.stopGame');
+	let answerOptionsBlock = $('.answerOptions');
+	let answerOptionsItem = $('.answerOptions > .but');
+	let notes = $('.notes');
+
+
+    stopButton.on('click', function() {
+    	answerOptionsBlock.hide();
+    	startButton.show();
+    	notes.hide();
+    });
+
+
+	let not = [];
+	not.push(notes);
+	
+	function getRandomInt(min, max) {
+	   min = Math.ceil(min);
+	   max = Math.floor(max);
+	   return Math.floor(Math.random() * (max - min)) + min;
+	}
+
+	let activeNotes = not[0][getRandomInt(0, 7)].id;
+	console.log(activeNotes)
+
+
+	answerOptionsBlock.show();
+	startButton.hide();
+
+	if(notes.is('#' + activeNotes)) {
+		$('#'+activeNotes).show();
+	}
+
+
+	$('.answerOptions >.but').on('click', function() {
+
+	  if($(this).attr("id") ===  activeNotes) {
+	  	console.log('esss')
+	  	notes.hide();
+	  	setTimeout(function() {
+	  		document.location.reload();
+	  	}, 1000);
+	 
+	  } else {
+	  	console.log('Noooooooo')
+	  }
+	})
+
+	
+
+}
