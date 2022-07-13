86913dbb9131f2c2168298ef523484f1cd2f3f62
Don Senior <stepan1q1@gmail.com>
2022-07-13 22:55:44 +0300

    added counter, js code fix, style

--- script.js
+++ script.js
@@ -4,18 +4,12 @@ function startGame() {
 	let startButton = $('.startGame');
 	let stopButton = $('.stopGame');
 	let answerOptionsBlock = $('.answerOptions');
-	let answerOptionsItem = $('.answerOptions > .but');
 	let notes = $('.notes');
-	let messageYes = $('.blockMessages > .yes');
-	let messageNo = $('.blockMessages > .nono');
-
+	let counter = $('.counterBlock > div');
 
     stopButton.on('click', function() {
-    	answerOptionsBlock.hide();
-    	startButton.show();
-    	notes.hide();
+		window.location.reload();
     });
-
 
 	let not = [];
 	not.push(notes);
@@ -29,7 +23,7 @@ function startGame() {
 	let activeNotes = not[0][getRandomInt(0, 13)].id;
 	console.log(activeNotes)
 
-
+	counter.show();
 	answerOptionsBlock.show();
 	startButton.hide();
 
@@ -47,30 +41,31 @@ function startGame() {
 		[2,9]
 	];
 
+	let counterBlockYes = $('.counterBlock > .counterYes > .textYes');
+	let counterBlockNo = $('.counterBlock > .counterNo > .textNo');
+	let counterYesPlus = 0;
+	let counterNoPlus = 0;
+
 	$('.answerOptions >.but').on('click', function() {
-		let background = $('.greyBackground');
-		let body = $('body');
 		let buttonID = $(this).attr("id");
 		buttonID --;
 
 		if (arr[buttonID] != 'undefined' && Array.isArray(arr[buttonID])) {
 			if (arr[buttonID].includes(Number(activeNotes))) {
 				notes.hide();
-				body.addClass('noScroll');
-				background.show();
-				messageYes.show();
+
+				counterYesPlus++;
+				counterBlockYes.html(counterYesPlus);
+
 				setTimeout(function() {
-					document.location.reload();
-				}, 1500);
+					activeNotes = not[0][getRandomInt(0, 13)].id;
+					if(notes.is('#' + activeNotes)) {
+						$('#'+activeNotes).show();
+					}
+				},100);
 			} else {
-				background.show();
-				messageNo.show();
-				body.addClass('noScroll')
-				setTimeout(function() {
-					messageNo.hide();
-					background.hide();
-					body.removeClass('noScroll');
-				}, 1500);
+				counterNoPlus++;
+				counterBlockNo.html(counterNoPlus);
 			}
 		}
 	})
